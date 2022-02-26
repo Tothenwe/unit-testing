f4db2e4a0ad7897a29d6025d706ab561c66a3041
Tothenwe <99398257+Tothenwe@users.noreply.github.com>
2022-02-26 22:41:44 +0530

    Update TestService.java

--- src/main/java/com/demo/service/TestService.java
+++ src/main/java/com/demo/service/TestService.java
@@ -1,7 +1,19 @@
 package com.demo.service;
 
 public class TestService {
+    OrderService orderService = OrderService.getInstance();
+
+	public int checkInstance() {
+		if (orderService != null)
+			return 1;
+		return 0;
+	}
     public int test() {
         return 1;
     }
+    public int checkPlaceOrder() {
+		if (orderService.placeOrder(new Order(), "abc"))
+			return 1;
+		return 0;
+	}
 }
